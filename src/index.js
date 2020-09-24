79070e877871b576b052ef08d3861373c8560bcc
Simone Carella <simonecarella@gmail.com>
2020-09-24 23:00:09 +0200

    *

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Canvas } from 'react-three-fiber'
-import Scene from './Scene'
-import './styles.css'
+import React from "react"
+import ReactDOM from "react-dom"
+import { Canvas } from "react-three-fiber"
+import Scene from "./Scene"
+import "./styles.css"
 
 function App() {
   return (
-    <Canvas>
+    <Canvas
+      camera={{
+        fov: 50,
+        position: [0, 0, 5]
+      }}>
       <Scene />
     </Canvas>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"))
