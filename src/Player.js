e799f4164d5fce37f8f1c02b1f07b4020b1aeb75
Simone Carella <simonecarella@gmail.com>
2020-11-19 15:21:34 +0100

    *

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react"
-import { useLoader, useThree, useUpdate } from "react-three-fiber"
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { useFrame, useLoader, useThree, useUpdate } from "react-three-fiber"
 import {
   AudioLoader,
   AudioListener,
@@ -18,16 +18,30 @@ import {
 // const fftSize = 512
 
 export default function Player({ side = "A", dataTexture, isPlaying = false }) {
-  useEffect(() => {
-    const audio = document.getElementById(side === "A" ? "trackA" : "trackB")
+  const willPause = useRef(false)
+  const audio = useMemo(
+    () => document.getElementById(side === "A" ? "trackA" : "trackB"),
+    [side]
+  )
 
+  useEffect(() => {
     if (isPlaying === 2) {
+      audio.volume = 0.9
       audio.play()
+      willPause.current = false
     } else {
+      willPause.current = true
+    }
+  }, [isPlaying, side])
+
+  useFrame(() => {
+    if (willPause.current && audio.volume > 0) {
+      audio.volume = Math.max(0, audio.volume - 0.015)
+    } else if (audio.volume <= 0) {
       audio.pause()
       audio.currentTime = 0
     }
-  }, [isPlaying, side])
+  })
 
   return null
 }
