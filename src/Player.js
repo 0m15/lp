2f5b1e8b92781c817557fe77dafa6b1ed4771492
Simone Carella <simonecarella@gmail.com>
2020-11-19 13:12:29 +0100

    fix playback

--- src/Player.js
+++ src/Player.js
@@ -19,53 +19,71 @@ const fftSize = 512
 let isUnlocked = false
 
 export default function Player({ side = "A", dataTexture, isPlaying = false }) {
+  // useEffect(() => {
+  //   // create empty buffer
+  //   window.addEventListener("touchstart", () => {
+  //     if (isUnlocked) return
+  //     var buffer = trackDummy.context.createBuffer(1, 1, 22050)
+  //     var source = trackDummy.context.createBufferSource()
+  //     source.buffer = buffer
+  //     source.connect(trackDummy.context.destination)
+  //     source.start(0)
+
+  //     // by checking the play state after some time, we know if we're really unlocked
+  //     setTimeout(function () {
+  //       if (
+  //         source.playbackState === source.PLAYING_STATE ||
+  //         source.playbackState === source.FINISHED_STATE
+  //       ) {
+  //         isUnlocked = true
+  //       }
+  //     }, 0)
+  //   })
+
+  //   loader.load("/niente-192.mp3", (buffer) => {
+  //     trackA.setBuffer(buffer)
+  //     trackA.setLoop(false)
+  //     trackA.setVolume(0.9)
+  //   })
+  //   loader.load("/due-192.mp3", (buffer) => {
+  //     trackB.setBuffer(buffer)
+  //     trackB.setLoop(false)
+  //     trackB.setVolume(0.9)
+  //   })
+  // }, [])
+
+  // useEffect(() => {
+  //   if (isPlaying) {
+  //     if (side === "A") {
+  //       if (trackB.isPlaying && trackB.hasPlaybackControl) trackB.stop()
+  //       trackA.play()
+  //     } else {
+  //       if (trackA.isPlaying && trackA.hasPlaybackControl) trackA.stop()
+  //       trackB.play()
+  //     }
+  //   } else {
+  //     trackA.isPlaying && trackA.hasPlaybackControl && trackA.stop()
+  //     trackB.isPlaying && trackB.hasPlaybackControl && trackB.stop()
+  //   }
+  // }, [side, isPlaying])
+
   useEffect(() => {
-    // create empty buffer
-    window.addEventListener("touchstart", () => {
-      if (isUnlocked) return
-      var buffer = trackDummy.context.createBuffer(1, 1, 22050)
-      var source = trackDummy.context.createBufferSource()
-      source.buffer = buffer
-      source.connect(trackDummy.context.destination)
-      source.start(0)
+    const audio = document.getElementById("trackA")
 
-      // by checking the play state after some time, we know if we're really unlocked
-      setTimeout(function () {
-        if (
-          source.playbackState === source.PLAYING_STATE ||
-          source.playbackState === source.FINISHED_STATE
-        ) {
-          isUnlocked = true
-        }
-      }, 0)
-    })
-
-    loader.load("/niente-192.mp3", (buffer) => {
-      trackA.setBuffer(buffer)
-      trackA.setLoop(false)
-      trackA.setVolume(0.9)
-    })
-    loader.load("/due-192.mp3", (buffer) => {
-      trackB.setBuffer(buffer)
-      trackB.setLoop(false)
-      trackB.setVolume(0.9)
-    })
-  }, [])
-
-  useEffect(() => {
-    if (isPlaying) {
-      if (side === "A") {
-        if (trackB.isPlaying && trackB.hasPlaybackControl) trackB.stop()
-        trackA.play()
+    const onClick = () => {
+      if (isPlaying) {
+        audio.play()
       } else {
-        if (trackA.isPlaying && trackA.hasPlaybackControl) trackA.stop()
-        trackB.play()
+        audio.pause()
+        audio.currentTime = 0
       }
-    } else {
-      trackA.isPlaying && trackA.hasPlaybackControl && trackA.stop()
-      trackB.isPlaying && trackB.hasPlaybackControl && trackB.stop()
     }
-  }, [side, isPlaying])
+    document.addEventListener("click", onClick)
+
+    return () => {
+      document.removeEventListener("click", onClick)
+    }
+  }, [isPlaying])
 
   return null
 }
